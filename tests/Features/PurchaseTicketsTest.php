1b0595f27e3af4fff6e45dccd7acdc97914c6c80
Kyle Cearley <kyle@ractoon.com>
2018-09-11 16:56:00 -0600

    Fake payment gateway

---
+++ tests/Features/PurchaseTicketsTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Concert;
+use App\Billing\PaymentGateway;
+use App\Billing\FakePaymentGateway;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class PurchaseTicketsTest extends TestCase
+{
+	use DatabaseMigrations;
+
+    /** @test */
+    public function customer_can_purchase_concert_tickets()
+    {
+    	$paymentGateway = new FakePaymentGateway;
+    	$this->app->instance(PaymentGateway::class, $paymentGateway);
+
+    	// Arrange
+    	// Create a concert
+    	$concert = factory(Concert::class)->create([
+    		'ticket_price' => 3250
+    	]);
+
+    	// Act
+    	// Purchase concert tickets
+    	$this->json('POST', "/concerts/{$concert->id}/orders", [
+    		'email' => 'john@example.com',
+    		'ticket_quantity' => 3,
+    		'payment_token' => $paymentGateway->getValidTestToken(),
+    	]);
+
+    	// Assert
+    	$this->assertResponseStatus(201);
+    	// Make sure the custom was charged the correct amount
+    	$this->assertEquals(9750, $paymentGateway->totalCharges());
+
+    	// Make sure that an order exists for this customer
+    	$order = $concert->orders()->where('email', 'john@example.com')->first();
+    	$this->assertNotNull($order);
+
+    	$this->assertEquals(3, $order->tickets->count());
+    }
+}
