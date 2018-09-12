947bb5b8bb6f479e2f47e805a14e15115ea05b3f
Kyle Cearley <kyle@ractoon.com>
2018-09-12 16:53:49 -0600

    Validation testing module

--- tests/Features/PurchaseTicketsTest.php
+++ tests/Features/PurchaseTicketsTest.php
@@ -11,12 +11,16 @@ class PurchaseTicketsTest extends TestCase
 {
 	use DatabaseMigrations;
 
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->paymentGateway = new FakePaymentGateway;
+        $this->app->instance(PaymentGateway::class, $this->paymentGateway);
+    }
+
     /** @test */
     public function customer_can_purchase_concert_tickets()
     {
-    	$paymentGateway = new FakePaymentGateway;
-    	$this->app->instance(PaymentGateway::class, $paymentGateway);
-
     	// Arrange
     	// Create a concert
     	$concert = factory(Concert::class)->create([
@@ -28,18 +32,33 @@ class PurchaseTicketsTest extends TestCase
     	$this->json('POST', "/concerts/{$concert->id}/orders", [
     		'email' => 'john@example.com',
     		'ticket_quantity' => 3,
-    		'payment_token' => $paymentGateway->getValidTestToken(),
+    		'payment_token' => $this->paymentGateway->getValidTestToken(),
     	]);
 
     	// Assert
     	$this->assertResponseStatus(201);
     	// Make sure the custom was charged the correct amount
-    	$this->assertEquals(9750, $paymentGateway->totalCharges());
+    	$this->assertEquals(9750, $this->paymentGateway->totalCharges());
 
     	// Make sure that an order exists for this customer
     	$order = $concert->orders()->where('email', 'john@example.com')->first();
     	$this->assertNotNull($order);
 
     	$this->assertEquals(3, $order->tickets->count());
+    }
+
+    /** @test */
+    public function email_is_required_to_purchase_tickets()
+    {
+        $concert = factory(Concert::class)->create();
+
+        $this->json('POST', "/concerts/{$concert->id}/orders", [
+            'ticket_quantity' => 3,
+            'payment_token' => $this->paymentGateway->getValidTestToken(),
+        ]);
+
+        $this->assertResponseStatus(422);
+
+        $this->assertArrayHasKey('email', $this->decodeResponseJson());
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -22,4 +22,15 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
 
         return $app;
     }
+
+    protected function disableExceptionHandling()
+    {
+        // $this->app->instance(ExceptionHandler::class, new class extends Handler {
+        //     public function __construct() {}
+        //     public function report(Exception $e) {}
+        //     public function render($request, Exception $e) {
+        //         throw $e;
+        //     }
+        // });
+    }
 }
